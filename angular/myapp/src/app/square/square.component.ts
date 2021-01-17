d625f3a9ba148a2d4a55d7bd8e171bfbe1d98cf1
Mike Thai <mta24@sfu.ca>
2021-01-16 22:40:37 -0800

    Add image to default squres

--- angular/myapp/src/app/square/square.component.ts
+++ angular/myapp/src/app/square/square.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton *ngIf="!value"><img src="assets/images/img.png" height="200"></button>
     <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
     <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
   `,
