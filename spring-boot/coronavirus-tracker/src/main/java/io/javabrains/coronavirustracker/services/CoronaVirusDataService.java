c39c074447fa734b90bef9c7b52359d45578e461
Mike Thai <mta24@sfu.ca>
2021-01-19 01:32:37 -0800

    Add model to set virus stats

--- spring-boot/coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ spring-boot/coronavirus-tracker/src/main/java/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,18 +1,20 @@
 package io.javabrains.coronavirustracker.services;
 
+import io.javabrains.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
@@ -22,7 +24,10 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
     @PostConstruct // when application starts, since this is a service, create instance of this class and execute this method
+    @Scheduled(cron = "* * 1 * * *") // run method once in the first hour of each day
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -31,7 +36,6 @@ public class CoronaVirusDataService {
 
         // send request to client by taking response body and return a string
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        System.out.println("http response " + httpResponse.body());
 
         parseCSV(httpResponse);
     }
@@ -40,10 +44,21 @@ public class CoronaVirusDataService {
         StringReader csvBodyReader = getStringReader(httpResponse);
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
 
+        setVirusData(records);
+    }
+
+    private void setVirusData(Iterable<CSVRecord> records) {
+        List<LocationStats> statsList = new ArrayList<>();
+
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            System.out.println(locationStat);
+            statsList.add(locationStat);
         }
+        this.allStats = statsList;
     }
 
     private StringReader getStringReader(HttpResponse<String> httpResponse) {
